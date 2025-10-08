4a598a078b8cce7f0af75bd7c62ae809deb78489
Eamonn Hynes <noreply@github.com>
2025-10-08 11:16:38 +0100

    Updated notes

--- hedera/tokenAddKycAccountId.ts
+++ hedera/tokenAddKycAccountId.ts
@@ -29,7 +29,7 @@ const tokenAddKycAccountId = async (tokenId: TokenId | string, accountId: Accoun
     }
 }
 
-const TOKEN_ID = '0.0.6975418'
+const TOKEN_ID = '0.0.6975423'
 const ACCOUNT_ID = '0.0.6941561'
 
 ;(async () => {
